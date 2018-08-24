60b7985ab8c70f6f41388c9af29e9250885f83cb
“zouye” <icrzye@126.com>
2018-08-24 13:20:12 +0800

    init files

---
+++ src/main/java/com/zouye/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.zouye.dao;
+
+import com.zouye.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
